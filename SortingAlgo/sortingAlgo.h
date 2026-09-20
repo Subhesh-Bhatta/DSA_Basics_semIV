90760dfa0020c690292782cc38f798b05e018e32
Subhesh-Bhatta <154715506+Subhesh-Bhatta@users.noreply.github.com>
2026-09-20 22:53:05 +0545

    Wrote proper declaration of mergeSort

--- SortingAlgo/sortingAlgo.h
+++ SortingAlgo/sortingAlgo.h
@@ -15,8 +15,8 @@ void quickSort(std::vector<int>& arr, int min=0, int max = -1, bool asc = true);
 // also caleld diminishing increment sort
 void shellSort(std::vector<int>& arr, bool asc = true);
 
-void heapSort(int arr[], int n);
+void mergeSort(std::vector<int>& arr, int left = 0, int right = -1, bool asc = true);
 
-void mergeSort(int arr[], int n);
+void heapSort(int arr[], int n);
 
 void radixSort(int arr[], int n);
