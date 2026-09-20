3a27b014fccc9f35f99cb2ef3d07590701c2f278
Subhesh-Bhatta <154715506+Subhesh-Bhatta@users.noreply.github.com>
2026-09-20 18:52:51 +0545

    Made the comment on line 11 be a TODO

--- SortingAlgo/sortingAlgo.h
+++ SortingAlgo/sortingAlgo.h
@@ -8,7 +8,7 @@ void selectionSort(std::vector<int>& arr, bool asc = true);
 void insertionSort(std::vector<int>& arr, bool asc = true);
 
 //also called partition-exchange sort
-//this is currently only Lorunto partioning scheme, make one for Hoare as well later
+//TODO: this is currently only Lorunto partioning scheme, make one for Hoare as well later
 void quickSort(std::vector<int>& arr, int min=0, int max = -1, bool asc = true);
 
 void shellSort(int arr[], int n);
