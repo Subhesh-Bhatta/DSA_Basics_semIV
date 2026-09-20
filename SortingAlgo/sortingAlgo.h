13d848a3a817cbd07438bc20ce6dfd1f3a59b38f
Subhesh-Bhatta <154715506+Subhesh-Bhatta@users.noreply.github.com>
2026-09-20 17:11:42 +0545

    Wrote the header about sorting algorithms i'll be practicing

---
+++ SortingAlgo/sortingAlgo.h
@@ -0,0 +1,17 @@
+#pragma once
+
+void bubbleSort(std::vector<int>& arr, bool asc = true);
+
+void selectionSort(std::vector<int>& arr, bool asc = true);
+
+void insertionSort(std::vector<int>& arr, bool asc = true);
+
+void quickSort(int arr[], int n); //also called partition-exchange sort
+
+void shellSort(int arr[], int n);
+
+void heapSort(int arr[], int n);
+
+void mergeSort(int arr[], int n);
+
+void radixSort(int arr[], int n);
